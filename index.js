de1826d03b71da374db4ec4e512748089589e535
mottox2 <y.mottox2@gmail.com>
2019-01-30 22:07:49 +0900

    Create plugin

---
+++ index.js
@@ -0,0 +1,21 @@
+const visit = require('unist-util-visit');
+
+module.exports = function(options) {
+  return (tree) => visit(tree, 'code', (node, index) => {
+    const [language, query] = (node.lang || '').split(':');
+    const { title } = qs.parse(query);
+    if (!title || !language) {
+      return;
+    }
+
+    const className = 'remark-code-title'
+
+    const titleNode = {
+      type: 'html',
+      value: `<div class="${className}">${title}</div>`.trim()
+    };
+
+    tree.children.splice(index, 0, titleNode);
+    node.lang = language;
+  });
+}
