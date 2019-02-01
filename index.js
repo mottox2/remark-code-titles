0d782c2e61e5203258a77e08ef6f725e35148a31
mottox2 <y.mottox2@gmail.com>
2019-02-01 19:55:39 +0900

    Change title format

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const visit = require('unist-util-visit');
-const qs = require('qs')
 
 module.exports = function(options) {
   return (tree) => visit(tree, 'code', (node, index) => {
-    const [language, query] = (node.lang || '').split(':');
-    const { title } = qs.parse(query);
-    if (!title || !language) {
+    const [language, title] = (node.lang || '').split(':');
+    if (!title) {
       return;
     }
 
